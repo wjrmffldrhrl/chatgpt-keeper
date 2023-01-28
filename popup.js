e74e2eb8b9c6592a661d2d8a98723d7fe7aaeb61
wjrmffldrhrl <wjrmffldrhrl@gmail.com>
2023-01-28 17:31:11 +0900

    First commit

---
+++ popup.js
@@ -0,0 +1,21 @@
+// popup.js
+
+// Get the save button
+const saveButton = document.getElementById('save-button');
+
+// Add a click event listener to the save button
+saveButton.addEventListener('click', saveData);
+
+// The saveData function
+function saveData() {
+    // Get the current tab
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        const activeTab = tabs[0];
+        const activeTabId = activeTab.id; // or do whatever you need
+
+        // Save the data to storage
+        chrome.storage.local.set({ 'activeTabId': activeTabId }, function () {
+            console.log('Data saved');
+        });
+    });
+}
