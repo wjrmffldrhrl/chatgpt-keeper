2d06f57a8a5e5d1bd3778e8f9d31f86f3471756c
wjrmffldrhrl <wjrmffldrhrl@gmail.com>
2023-01-28 20:36:03 +0900

    Add download

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 // Create the button and add an id
 var button = document.createElement("button");
 button.innerHTML = "Save Chat";
@@ -14,8 +15,39 @@ document.body.appendChild(buttonDiv);
 // Add event listener for button click
 document.getElementById("chatGPT-save-button").addEventListener("click", saveChat);
 
+
 // Function to handle button click
 function saveChat() {
     console.log("Chat saved!");
     // Add code here to save the chat
+
+    var messageBoxes = document.querySelectorAll('.flex.flex-grow.flex-col.gap-3');
+
+    let finalText = "";
+    let currentSpeaker = "You";
+
+
+    messageBoxes.forEach(function (box) {
+        finalText += "### " + currentSpeaker + "\n" + box.textContent + "\n\n";
+        if (currentSpeaker === "You") {
+            currentSpeaker = "chatGPT";
+        } else {
+            currentSpeaker = "You";
+        }
+    });
+
+
+    let downloadUrl = URL.createObjectURL(new Blob([finalText], { type: 'application/octet-binary' }));
+    
+    // Create an <a> tag
+    var a = document.createElement("a");
+
+    // Set the href of the <a> tag to the URL of the Blob object
+    a.href = downloadUrl;
+
+    // Set the download attribute of the <a> tag to the desired file name
+    a.download = "chat.md";
+
+    // Simulate a click on the <a> tag to initiate the download
+    a.click();
 }
