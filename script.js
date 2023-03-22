29384a018f0aa4f67e2f9ff629d3353ffa0933b0
Takanori Suzuki <mail.tks@gmail.com>
2023-03-22 10:38:37 +0900

    Add more markdown conversion by using Showdown
    - Showdown URL: https://github.com/showdownjs/showdown/blob/95255984ad80acf745ed74605bd3ad8357dc9b33/dist/showdown.js

--- script.js
+++ script.js
@@ -15,6 +15,27 @@ document.body.appendChild(buttonDiv);
 // Add event listener for button click
 document.getElementById("chatGPT-save-button").addEventListener("click", saveChat);
 
+function unwrapElements(element, tagName) {
+  const elements = element.querySelectorAll(tagName);
+  elements.forEach(el => {
+    while (el.firstChild) {
+      el.parentNode.insertBefore(el.firstChild, el);
+    }
+    el.parentNode.removeChild(el);
+  });
+}
+
+function htmlToMarkdownCodeBlock(element) {
+  const elements = element.querySelectorAll('pre');
+  elements.forEach(el => {
+    const codeElement = el.querySelector('code');
+    const language = codeElement.closest('pre').querySelector('span').textContent.trim();
+    const code = codeElement.textContent.trim();
+
+    el.innerHTML = `<code class="${language} language-${language}">${code}</code>\n`;
+  });
+}
+
 
 // Function to handle button click
 function saveChat() {
@@ -29,10 +50,19 @@ function saveChat() {
 
 
     messageBoxes.forEach(function (box) {
-        finalText += "### " + currentSpeaker + "\n" + box.textContent + "\n\n";
         if (currentSpeaker === "You") {
+            finalText += "### " + currentSpeaker + "\n" + box.textContent + "\n\n";
+
             currentSpeaker = "chatGPT";
         } else {
+            let box_clone = box.cloneNode(true);
+            htmlToMarkdownCodeBlock(box_clone);
+            unwrapElements(box_clone, "div");
+            let boxHtml = box_clone.innerHTML;
+            const converter = new showdown.Converter();
+            let markdown = converter.makeMarkdown(boxHtml);
+            finalText += "### " + currentSpeaker + "\n" + markdown + "\n\n";
+
             currentSpeaker = "You";
         }
     });
